a00599275cce41d3a35e204d5a8412d9c1a30b5a
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-05-17 11:47:21 +0300

    Clean up

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -23,32 +23,6 @@ class DashboardController extends Controller
 
         // an admin
         return view('dashboard.admin', compact('facilitators', 'centers', 'trainings', 'trainees', 'projects'));
-
-        // if (auth()->user()->isAdmin()) {
-        //     $teachers = User::all()->where('role', 'Teacher');
-        //     $facilitators = User::all()->where('role', 'Facilitator');
-        //     $schools = School::all();
-        //     $lessonPlans = lessonPlan::all();
-        //     $steps = lessonStep::all();
-
-        //     // an admin
-        //     return view('dashboard.admin', compact('teachers', 'facilitators', 'schools', 'lessonPlans', 'steps'));
-
-        // }else{
-        //     //a teacher
-        //     $teachers = User::all()->where('role', 'Teacher');
-        //     $facilitators = User::all()->where('role', 'Facilitator');
-        //     $schools = School::all();
-        //     $subjects = Subject::all();
-        //     $lessonPlans = LessonPlan::all();
-        //     $yourLPs = LessonPlan::all()->where('owner', auth()->user()->id);
-        //     $yourApprovedLPs = LessonPlan::all()->where('owner', auth()->user()->id)
-        //                                 ->where('status', 'approved');
-        //     $steps = LessonStep::all();
-
-        //     // an admin
-        //     return view('dashboard.teacher', compact('teachers', 'facilitators', 'schools', 'subjects', 'lessonPlans', 'yourLPs', 'yourApprovedLPs', 'steps'));
-        // }
     }
 
     public static function getAttendanceForTraining($training, $startDate, $endDate)
